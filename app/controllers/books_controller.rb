60ce8c574d70b2be0df5df0810d65a15d432308c
williamhsu17 <williamhsu17@gmail.com>
2016-11-08 18:11:16 +0800

    CRUD finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   end
 
   def new
-
+    @book = Book.new
   end
 
   def show
@@ -13,12 +13,33 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new( book_params )
+    if @book.save
+      redirect_to books_path
+      flash[:notice] = "新增成功"
+    else
+      render new_book_path
+    end
+  end
 
+  def update
+    if @book.update( book_params )
+      redirect_to book_path(@book)
+      flash[:notice] = "更新成功"
+    else
+      render edit_book_path(@book)
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+    flash[:alert] = "刪除成功"
   end
 
   private
     def book_params
-
+      params.require(:book).permit(:name, :author, :description)
     end
     def set_book
       @book = Book.find(params[:id])
